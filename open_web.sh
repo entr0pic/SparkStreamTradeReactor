7b2926561acaf1ac7592c737dce5380374351f07
Adam Parker <adam.parker@servian.com.au>
2015-07-31 01:46:46 +1000

    Updated web script and notebooks

--- open_web.sh
+++ open_web.sh
@@ -3,3 +3,4 @@ open http://`docker-machine ip dev4g`:8081
 open http://`docker-machine ip dev4g`:50070
 open http://`docker-machine ip dev4g`:8888
 open http://`docker-machine ip dev4g`:9000
+open http://`docker-machine ip dev4g`:8090
