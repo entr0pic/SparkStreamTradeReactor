f70746564d75b5d5b562e57ec0aaedcf1beab69e
Olena Aleksieieva <olena.aleksieieva@servian.com.au>
2015-08-11 19:28:20 +1000

    K clusters - try number 2, fully streaming version, stream print added, clean and train data inited

--- SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
+++ SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
@@ -130,11 +130,11 @@ def CreateDataArray(src: Array[String]) : Array[Double] = {
       
  val cleanData = messages.map{
      case(_,line) => line.split(",")
- }.map(CreateDataArray(_)).cache()
+ }.flatMap(CreateDataArray(_))
 
       
 //      var testingData = cleanData.map(l => LabeledPoint(l(0), l)).map(LabeledPoint.parse)
-      var trainingData = cleanData.map(Vectors.parse)
+      var trainingData = cleanData.map(x => Vectors.parse(x))
 //
 //      val numClusters = 34
 //      var numDimensions = 3
