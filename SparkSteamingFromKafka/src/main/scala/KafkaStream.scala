1cc306b99b045795726f41d8824044ef988ea50c
Olena Aleksieieva <olena.aleksieieva@servian.com.au>
2015-08-13 12:10:23 +1000

    Print parsed maps

--- SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
+++ SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
@@ -152,8 +152,8 @@ def CreateDataArray(src: Map[String,Any]) : Array[Any] = {
  }
     
 //     cleanData1.print()
-//val cleanData = cleanData1.map(CreateDataArray(_))
-cleanData1.flatMap(CreateDataArray).print()
+//val cleanData = cleanData1.map(x => CreateDataArray(x)).print()
+cleanData1.flatMap(x => CreateDataArray(x)).print()
       
 //val trainingData = cleanData.map(_.take(4)).flatMap(x => x.map(_.toDouble))//.map(Vectors.parse)
 //trainingData.print()
