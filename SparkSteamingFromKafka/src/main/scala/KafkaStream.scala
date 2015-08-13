05e53f4b9d5cdcf64a8ff1601cc33fdd5b291d5e
Olena Aleksieieva <olena.aleksieieva@servian.com.au>
2015-08-13 15:33:15 +1000

    parse input maps

--- SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
+++ SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
@@ -147,16 +147,11 @@ def CreateDataArray(src: Map[String,Any]) : Array[Any] = {
       
  val cleanData1 = messages.map{
 //     case(_,line) => line.split(",")//.map(x => x.split(":"))
-     case (_,line) => {
-         JSON.parseFull(line) match {
-            case Some(x) => x
-             //case _ => _
-         }
-     }
+     case (_,line) => JSON.parseFull(line).flatMap(CreateDataArray(_))
  }
     
-//cleanData1.print()
-val cleanData = cleanData1.flatMap(x => CreateDataArray(x)).print()
+cleanData1.print()
+//val cleanData = cleanData1.flatMap(x => CreateDataArray(x)).print()
       
 //val trainingData = cleanData.map(_.take(4)).flatMap(x => x.map(_.toDouble))//.map(Vectors.parse)
 //trainingData.print()
