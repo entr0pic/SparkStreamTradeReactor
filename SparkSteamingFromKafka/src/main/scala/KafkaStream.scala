7511a6f28479d82c91798192b2fe6b8af5f3c459
Olena Aleksieieva <olena.aleksieieva@servian.com.au>
2015-08-18 10:44:16 +1000

    try no 3

--- SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
+++ SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
@@ -143,10 +143,12 @@ def CreateDataArray(src: Map[String,Any]) : Array[Any] = {
       
       
  val cleanData = messages.map(
-     (_,line) => JSON.parseFull(line) match {
-      case Some(x) => CreateDataArray(x)
-      case None => new Array[Any](0)
-    }
+     case (_,line) => { 
+         JSON.parseFull(line) match {
+          case Some(x) => CreateDataArray(x)
+          case None => new Array[Any](1)
+        }
+     }
  )//.filter(_.size>1)
     
 cleanData.print()
