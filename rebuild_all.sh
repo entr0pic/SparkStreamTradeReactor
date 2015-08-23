d4c9992d020e0cce2186083c231dc6c8cc9c36b3
Olena Aleksieieva <olena.aleksieieva@servian.com.au>
2015-08-24 00:49:29 +1000

    moved kafka (submitter) to start after trade generator

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -9,12 +9,12 @@ docker build -t localhost:5000/base Base/.
 #docker push localhost:5000/base
 docker build -t localhost:5000/hadoop Hadoop/.
 #docker push localhost:5000/hadoop
+docker build -t localhost:5000/kafka Kafka/.
+#docker push localhost:5000/kafka
 docker build -t localhost:5000/spark Spark/.
 #docker push localhost:5000/spark
 docker build -t localhost:5000/tradegenerator TradeGenerator/.
 #docker push localhost:5000/tradegenerator
-docker build -t localhost:5000/kafka Kafka/.
-#docker push localhost:5000/kafka
 docker build -t localhost:5000/zeppelin Zeppelin/.
 #docker push localhost:5000/zeppelin
 
