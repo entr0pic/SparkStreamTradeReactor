0d69c5975cfa239cd44e1f03b0f4325037dcc2e3
Olena Aleksieieva <olena.aleksieieva@servian.com.au>
2015-09-18 06:23:40 +1000

    contexxt name bugfix

--- SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
+++ SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
@@ -299,7 +299,7 @@ try {
 //    tvectors.count()  // Calls an action to create the cache.
 //
 //    sModel.predictOnValues(tvectors).print()
-    sc.makeRDD(sModel.clusterCenters, numClusters).saveAsObjectFile("/model")
+    ssc.makeRDD(sModel.clusterCenters, numClusters).saveAsObjectFile("/model")
 
 //        .transform { rdd =>
 //            try{
