7c524891780f29dc81538afefd45ade3d1899829
Adam <parker909@gmail.com>
2015-10-19 22:40:04 +1100

    Trade consumer automated retry if topics don't exist yet

--- TradeGenerator/app/TradeConsumer.js
+++ TradeGenerator/app/TradeConsumer.js
@@ -10,14 +10,9 @@ topicNames = topicNames.split(',').map(function(d) {return d.trim(); });
 
 var kafka = require('kafka-node'),
     Consumer = kafka.Consumer,
-    client = new kafka.Client(kafkaLocation+':2181','trade-consumer'),
-    consumer = new Consumer(
-        client,
-        topicNames.map(function(d) {return {topic: d}}),
-        {
-            autoCommit: false
-        }
-    );
+    client = new kafka.Client(kafkaLocation+':2181','trade-consumer');
+
+
 console.log("Subscribing to topics:");
 console.log(topicNames.map(function(d) {return {topic: d}}));
 
@@ -32,24 +27,33 @@ app.get('/', function(req, res){
 var messageCount = 0;
 var topicCount = topicNames.map(function(d){var obj = {}; obj[d] = 0; return obj;});
 
-
- consumer.on('message', function (message) {
-   console.log(message);
-     try {
-        if (message.value) {
-            io.emit(message.topic, message);
-            process.stdout.write("Received " + (messageCount++) + "(" + (topicCount[message.topic]++) + ")" + " ["+message.topic+"] messages: " + message.value + "\r");
-
-        } else {
-            process.stdout.write("Message " + message);
+var startConsuming = function() {
+  var consumer = new Consumer(
+        client,
+        topicNames.map(function(d) {return {topic: d}}),
+        {
+            autoCommit: false
         }
-    } catch (error) {
-        process.stderr.write("Err ", error);
-    }
-}).on("error",function(e) {
-  console.log("Err: ", e);
-});
+    );
+  consumer.on('message', function (message) {
+   console.log(message);
+       try {
+          if (message.value) {
+              io.emit(message.topic, message);
+              process.stdout.write("Received " + (messageCount++) + "(" + (topicCount[message.topic]++) + ")" + " ["+message.topic+"] messages: " + message.value + "\r");
 
+          } else {
+              process.stdout.write("Message " + message);
+          }
+      } catch (error) {
+          process.stderr.write("Err ", error);
+      }
+  }).on("error",function(e) {
+    console.log("Err: ", e);
+    console.log('Retrying in 2 seconds...');
+    setTimeout(startConsuming,2000);
+  });
+};
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
