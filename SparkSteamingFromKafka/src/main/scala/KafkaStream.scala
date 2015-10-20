cdbaed37ca7b20d574c1fab267dceaedcacddd19
Olena Aleksieieva <olena.aleksieieva@servian.com.au>
2015-10-20 19:21:50 +1100

    unicode parse

--- SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
+++ SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
@@ -148,7 +148,7 @@ def getStringByWeight(a: Double) : String = {
 
 def getUnicode(s1:String, s2:String, s3:String) : String = {
 //    val ret = """\""" +"u0" + s1 + s2 + s3
-    (s1+s2+s3).toInt.toChar.toString
+    Integer.parseInt(s1+s2+s3, 16).toChar.toString
 }
 
 /*
