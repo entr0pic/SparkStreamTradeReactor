899a79aa8fa790e3f3b5bf3a5f1280b391626318
Olena Aleksieieva <olena.aleksieieva@servian.com.au>
2015-10-20 17:39:21 +1100

    exit debug

--- SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
+++ SparkSteamingFromKafka/src/main/scala/KafkaStream.scala
@@ -71,7 +71,6 @@ object TradeStreamReader {
     val sparkConf = new SparkConf().setAppName("TradeStreamReader")
     val ssc = new StreamingContext(sparkConf, Seconds(5))
     //val sqlContext = new SQLContext(sc)
-      System.exit(0)
 
     // Create direct kafka stream with brokers and topics
     val topicsSet = topics.split(",").toSet
@@ -200,6 +199,7 @@ trades.count().print
 //messages.flatMap{case (_,line) => line}.foreach(a => println(a))
 //trades.print()
 //ttrades.print()
+      System.exit(0)
 
 try {
     /*
